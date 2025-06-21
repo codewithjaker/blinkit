d2b428684d87f4279837e6cc707fec2b26f93195
codewithjaker <mohamadjakerhossain86@gmail.com>
2025-06-21 21:56:38 +0600

    env is remove

---
+++ server/utils/verifyEmailTemplate.js
@@ -0,0 +1,10 @@
+const verifyEmailTemplate = ({ name, url }) => {
+    return
+    `
+        <p>Dear ${name}</p>
+        <p>Thank you for register Blinkit</p>
+        <a href=${url} style='color:white;background:blue;margin-top:10px'>Verify Email</a>
+    `
+}
+
+export default verifyEmailTemplate
